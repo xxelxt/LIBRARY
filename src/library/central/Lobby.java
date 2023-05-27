6813e055ea580b371b29ba6e890da2a608961d82
xxelxt <elt.7713@gmail.com>
2023-05-27 16:01:35 +0700

    Update

---
+++ src/library/central/Lobby.java
@@ -0,0 +1,20 @@
+package library.central;
+
+import java.util.ArrayList;
+
+import library.publication.Books;
+import library.publication.PrintMedia;
+import library.user.Librarian;
+import library.user.Manager;
+import library.user.Users;
+
+public class Lobby {
+    public static ArrayList<Users> UsersList;
+    public static ArrayList<Books> BooksList;
+    public static ArrayList<PrintMedia> PrintMediaList;
+    public static ArrayList<Librarian> LibrarianList;
+    public static ArrayList<Manager> ManagerList;
+    public static ArrayList<Borrow> BorrowList;
+
+    
+}
