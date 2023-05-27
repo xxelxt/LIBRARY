4a74e84c7ea79a8dacf81e7f4a652e7e503ff16d
xxelxt <elt.7713@gmail.com>
2023-05-27 14:54:45 +0700

    Update Student.java

--- src/library/user/Student.java
+++ src/library/user/Student.java
@@ -5,24 +5,42 @@ public class Student extends Person {
 	private String ClassName;
 	private double Fine;
 
-	public Student(String Username, String Password, String Name, String Gender, String Address, String Phone, String StudentID, String Class, double Fine) {
+	public Student(String Username, String Password, String Name, String Gender, String Address, String Phone, String StudentID, String ClassName, double Fine) {
 		super(Username, Password, Name, Gender, Address, Phone);
 		this.StudentID = StudentID;
 		this.ClassName = ClassName;
 		this.Fine = Fine;
 	}
+	
+	
+	
 
 	public String getStudentID() {
 		return this.StudentID;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	public void setStudentID(String StudentID) {
 		this.StudentID = StudentID;
 	}
-	
-	
-	
-	
 
 	public String getClassName() {
 		return this.ClassName;
