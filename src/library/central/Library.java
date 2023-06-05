ce30a684e8c6b1883589051d90e7a448bd1b6904
xxelxt <elt.7713@gmail.com>
2023-06-05 16:00:31 +0700

    Update Library.java

--- src/library/central/Library.java
+++ src/library/central/Library.java
@@ -2,6 +2,7 @@ package library.central;
 
 import java.util.ArrayList;
 
+import library.mysql.Database;
 import library.publication.Books;
 import library.publication.PrintMedia;
 import library.user.Clerk;
@@ -20,4 +21,53 @@ public class Library {
     public static ArrayList<Borrow> BorrowList;
     public static ArrayList<Event> EventList;
     
+    public Library() {
+        Database db = new Database();
+        
+    //  UsersList = db.loadAllUsers();
+        StudentList = db.loadAllStudents();
+        BooksList = db.loadAllBooks();
+        PrintMediaList = db.loadAllPrintMedias();
+
+        LibrarianList = db.loadAllLibrarians();
+        ClerkList = db.loadAllClerks();
+        BorrowList = db.loadBorrowList();
+    //  EventList = db.loadEventList();
+
+        for (int i = 0; i < StudentList.size(); i++) {
+            Student S = StudentList.get(i);
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < BooksList.size(); i++) {
+            Books B = BooksList.get(i);
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < PrintMediaList.size(); i++) {
+            PrintMedia PM = PrintMediaList.get(i);
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < LibrarianList.size(); i++) {
+            Librarian L = LibrarianList.get(i);
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < ClerkList.size(); i++) {
+            Clerk C = ClerkList.get(i);
+
+            System.out.println();
+        }
+
+        for (int i = 0; i < BorrowList.size(); i++) {
+            Borrow B = BorrowList.get(i);
+
+            System.out.println();
+        }
+    }
 }
