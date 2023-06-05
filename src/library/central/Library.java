c00b6a857a70f8c42508fe48f9e9de692c1cc224
xxelxt <elt.7713@gmail.com>
2023-06-05 15:54:58 +0700

    Update Library.java

--- src/library/central/Library.java
+++ src/library/central/Library.java
@@ -1,5 +1,23 @@
 package library.central;
 
+import java.util.ArrayList;
+
+import library.publication.Books;
+import library.publication.PrintMedia;
+import library.user.Clerk;
+import library.user.Librarian;
+import library.user.Student;
+import library.user.User;
+
 public class Library {
+
+    public static ArrayList<User> UsersList;
+    public static ArrayList<Student> StudentList;
+    public static ArrayList<Books> BooksList;
+    public static ArrayList<PrintMedia> PrintMediaList;
+    public static ArrayList<Librarian> LibrarianList;
+    public static ArrayList<Clerk> ClerkList;
+    public static ArrayList<Borrow> BorrowList;
+    public static ArrayList<Event> EventList;
     
 }
