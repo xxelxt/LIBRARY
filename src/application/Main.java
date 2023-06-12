d32ccea57aa0af1cc5383f90eeac0eeca07ea5b2
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2023-06-12 18:05:00 +0800

    added first UI

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+	        Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+	        primaryStage.setTitle("Registration Form FXML Application");
+	        
+	        Scene scene = new Scene(root, 800, 500);
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	        
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
