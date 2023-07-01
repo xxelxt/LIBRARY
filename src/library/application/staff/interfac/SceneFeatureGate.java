86bc4be636cca2938367f20b83694dcf3716b330
hwangisgone <95560238+hwangisgone@users.noreply.github.com>
2023-07-01 19:51:28 +0800

    test

--- src/library/application/staff/interfac/SceneFeatureGate.java
+++
@@ -1,8 +0,0 @@
-package library.application.staff.interfac;
-
-public interface SceneFeatureGate {
-	public static final int LIBRARIAN = 1;
-	public static final int CLERK = 2;
-	public static final int STUDENT = 3;
-	public void setFeatureFor(Integer user);
-}
